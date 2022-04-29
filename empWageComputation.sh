2751f239521b10a8c3d5b66de0707e5c27ceb330
nanawarechaitali <nanawarechaitali4196@gamil.com>
2022-04-29 07:56:01 +0530

    [Chaitali] : Added  DailyWage Using SwitchCase in Employee_Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -26,3 +26,25 @@ echo "$DailyWage"
 
 #UC3
 Part_Time_Hour=4
+
+
+
+
+
+
+
+
+#UC4
+Choice=$((1+$RANDOM%2))
+case $Choice in 
+        1)Emp_Hr=8;;
+        2)Emp_Hr=4;;
+        *)
+esac
+
+
+Daily_wage=$(( $Emp_Hr * $Wage_Per_Hour ))
+
+
+
+echo "Daily wage using switch case" $Daily_Wage
